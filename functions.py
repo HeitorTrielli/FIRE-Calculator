93bb75081a4cdbbc13a60e59da4a395f0be3840e
heitort <heitor.trielli@quantumfinance.com.br>
2025-01-31 16:57:16 -0300

    backup

--- functions.py
+++ functions.py
@@ -1,12 +1,15 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
+from matplotlib.ticker import MultipleLocator
 
 
-def generate_ar1_timeseries(initial_return, years, phi=0.999, sigma=0.005):
+def generate_ar1_timeseries(
+    initial_return, num_years_simulated, phi=0.999, sigma=0.005
+):
     initial_return = initial_return - 1
     returns = [initial_return]
-    for _ in range(1, years):
+    for _ in range(1, num_years_simulated):
         noise = np.random.normal(0, sigma)
         new_return = phi * returns[-1] + noise
         returns.append(new_return)
@@ -17,61 +20,67 @@ def generate_ar1_timeseries(initial_return, years, phi=0.999, sigma=0.005):
 
 
 def get_wealth(
-    years,
+    num_years_simulated,
     retirement_year,
     yearly_wage,
-    yr_allow,
-    cost_coverage,
-    fixed_yearly_cost,
+    yearly_fixed_cost,
     yearly_real_rate,
-    wage_growth_rate,
-    m0,
+    non_wage_income=0,
+    initial_capital=0,
+    wage_growth_rate=0,
     yearly_returns=None,
 ):
-
-    fixed_yearly_cost_covered = fixed_yearly_cost * (1 - cost_coverage)
-    yearly_allowance = 50_000 * yr_allow
-    yearly_savings = yearly_allowance + yearly_wage - fixed_yearly_cost_covered
-
-    total = m0
+    total = initial_capital
     found_breakeven = False
     million_mult = 1
+    first_million = False
+
+    yearly_income = yearly_wage + non_wage_income
 
     list_year_total = []
     list_year_income = []
     breakeven_year = -1
-    for i in range(int(years)):
+    for i in range(num_years_simulated):
         if yearly_returns:
             return_rate = yearly_returns[i]
-            print(f"On year {i+1} we had the return rate of: {return_rate}")
+            print(f"On year {i+1} we had the return rate of: {return_rate}%")
         else:
             return_rate = yearly_real_rate
 
-        wage_growth = yearly_wage * (wage_growth_rate - 1)
+        wage_growth = yearly_wage * wage_growth_rate
         yearly_wage += wage_growth
-        yearly_savings += wage_growth
+        yearly_income += wage_growth
         if i == retirement_year - 1:
-            yearly_savings = yearly_savings - yearly_wage
+            print(f"After {retirement_year} years, you are finally retired!")
+            yearly_income = non_wage_income
             yearly_wage = 0
 
-        total += total * (return_rate - 1) + yearly_savings
+        total += total * return_rate + yearly_income - yearly_fixed_cost
 
-        yearly_income_from_savings = total * (1.06 - 1)
+        yearly_income_from_savings = total * yearly_real_rate
 
         list_year_total.append((i + 1, total))
         list_year_income.append((i + 1, yearly_income_from_savings))
 
-        if int(total / 1_000_000) > million_mult:
-            print(f"On year {i+1} you got to ${int(total / 1_000_000)} million dollars")
+        if int(total / 1_000_000) > million_mult or (
+            not first_million and total >= 1_000_000
+        ):
+            first_million = True
+            print(
+                f"In {i+1} years you accumulated to ${int(total / 1_000_000)} million dollars"
+            )
             million_mult = int(total / 1_000_000)
 
-        if yearly_income_from_savings > fixed_yearly_cost and not found_breakeven:
+        if (
+            yearly_income_from_savings + non_wage_income > yearly_fixed_cost
+            and not found_breakeven
+        ):
             found_breakeven = True
             breakeven_year = i + 1
             print("Break even at year:", breakeven_year)
 
     print(
-        f"You accumulated over the {years} years of saving {yearly_savings:,}/year at a real rate of {yearly_real_rate}: {round(total, ndigits = 2):,}"
+        f"You accumulated over the {num_years_simulated} num_years_simulated of saving {yearly_income-yearly_fixed_cost:,}/year at a real rate of {yearly_real_rate}: {round(total, ndigits = 2):,}"
     )
     print(
         f"This will give you a real income (yet to be taxed) of: {round(yearly_income_from_savings, ndigits = 2):,}"
@@ -86,7 +95,7 @@ def get_wealth(
 def plot_multiple_projections(
     retirement_years,
     num_simulation,
-    fixed_yearly_cost,
+    yearly_fixed_cost,
     total_years=35,
     alpha=0.05,
     initial_return_rate=1.06,
@@ -118,7 +127,7 @@ def plot_multiple_projections(
             year_total_df, year_income_df, breakeven_year = get_wealth(
                 yearly_returns=yearly_returns,
                 retirement_year=retirement_year,
-                years=total_years,
+                num_years_simulated=total_years,
             )
 
             plt.plot(
@@ -134,7 +143,7 @@ def plot_multiple_projections(
 
             # Add fixed yearly cost line
             plt.axhline(
-                y=fixed_yearly_cost, color="r", linestyle="--", label="Cost Line"
+                y=yearly_fixed_cost, color="r", linestyle="--", label="Cost Line"
             )
 
     # Formatting the plot
@@ -146,10 +155,28 @@ def plot_multiple_projections(
     plt.show()
 
 
-def plot_retirement_comparison(list_retirement):
+def plot_retirement_comparison(
+    list_retirement: list[int],
+    num_years_simulated: int,
+    yearly_wage: float,
+    yearly_fixed_cost: float,
+    yearly_real_rate: float,
+    non_wage_income: float = 0.0,
+    initial_capital: float = 0.0,
+    wage_growth_rate: float = 0.0,
+    yearly_returns: list[float] | None = None,
+):
     for retirement_year in list_retirement:
         year_total_df, year_income_df, breakeven_year = get_wealth(
-            retirement_year=retirement_year
+            num_years_simulated,
+            retirement_year=retirement_year,
+            yearly_wage=yearly_wage,
+            yearly_fixed_cost=yearly_fixed_cost,
+            yearly_real_rate=yearly_real_rate,
+            non_wage_income=non_wage_income,
+            initial_capital=initial_capital,
+            wage_growth_rate=wage_growth_rate,
+            yearly_returns=yearly_returns,
         )
         plt.plot(
             year_income_df.year,
@@ -157,9 +184,12 @@ def plot_retirement_comparison(list_retirement):
             label=f"Retirement at year {retirement_year}",
         )
 
-        plt.axhline(y=fixed_yearly_cost, color="r", linestyle="--")
-        plt.axvline(x=breakeven_year, color="g", linestyle="--")
-        plt.ticklabel_format(style="plain", axis="y")
+        if breakeven_year > 0:
+            plt.axvline(x=breakeven_year, linestyle="--")
+            plt.ticklabel_format(style="plain", axis="y")
 
+    plt.axhline(y=yearly_fixed_cost, color="r", linestyle="--")
     plt.legend()
+    plt.grid(color="gray", linestyle="--", linewidth=0.5)
+    plt.gca().xaxis.set_major_locator(MultipleLocator(1))
     plt.show()
--- your_future_calculator.py
+++ your_future_calculator.py
@@ -1,31 +1,43 @@
-from functions import get_wealth
+from functions import get_wealth, plot_retirement_comparison
 
-years = 15
-retirement_year = 255
+num_years_simulated = 35
+retirement_year = 15
 doc = 1
 mt = 1
 yr_allow = 1
 cost_coverage = 0
-fixed_yearly_cost = 4500 * 12 * 0
+fixed_yearly_cost = 5500 * 12
 
-yearly_real_rate = 1.06
-wage_growth_rate = 1.0
+yearly_real_rate = 0.06
+wage_growth_rate = 0.0
 
+m0 = 180_000
 
-m0 = 151_000
+yearly_fixed_cost = fixed_yearly_cost * (1 - cost_coverage)
+yearly_allowance = 50_000 * yr_allow
 yearly_wage = (10000 * 15) * mt + 4000 * 12 * doc
 
+
 _ = get_wealth(
-    years=years,
+    num_years_simulated=num_years_simulated,
     retirement_year=retirement_year,
     yearly_wage=yearly_wage,
-    yr_allow=yr_allow,
-    cost_coverage=cost_coverage,
-    fixed_yearly_cost=fixed_yearly_cost,
+    non_wage_income=yearly_allowance,
+    yearly_fixed_cost=yearly_fixed_cost,
     yearly_real_rate=yearly_real_rate + 0.0,
     wage_growth_rate=wage_growth_rate,
-    m0=m0,
+    initial_capital=m0,
     yearly_returns=None,
 )
 
-# plot_retirement_comparison([8, 15])
+plot_retirement_comparison(
+    [7, 15],
+    num_years_simulated=num_years_simulated,
+    yearly_wage=yearly_wage,
+    non_wage_income=yearly_allowance,
+    yearly_fixed_cost=yearly_fixed_cost,
+    yearly_real_rate=yearly_real_rate + 0.0,
+    wage_growth_rate=wage_growth_rate,
+    initial_capital=m0,
+    yearly_returns=None,
+)
